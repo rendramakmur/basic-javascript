3d0da20123ada7f68b42287d79cbe0a68c7f2c87
Rendra <rendra1795@gmail.com>
2020-12-21 23:32:26 +0700

    Belajar fungsi forEach, map, filter, find

--- array/manipulasiArray/manipulasiArray.js
+++ array/manipulasiArray/manipulasiArray.js
@@ -39,7 +39,7 @@
 
 // 4. Splice (Menyatukan) atau menginsert di tengah2 array.
 // splice(indexStartnyaDiBerapa, mauDiHapusBerapa, elemenBaru1, elemenBaru2, ...)
-var arr = ['Galuh', 'Faiz', 'Mekel', 'Yuli', 'Ferry'];
+// var arr = ['Galuh', 'Faiz', 'Mekel', 'Yuli', 'Ferry'];
 
 // Mulainya di Index 2, Delete 0 array, tambah Acis Rendra
 // arr.splice(2, 0, 'Acis', 'Rendra')
@@ -52,7 +52,69 @@ var arr = ['Galuh', 'Faiz', 'Mekel', 'Yuli', 'Ferry'];
 // 5. Slice (Memotong untuk dibuat array baru)
 // slice(awal, akhir) || akhir disini ga diambil, jadi range indexnya antara awal sampai (akhir - 1).
 // var arr2 = arr.slice(3, 5);
-var arr2 = arr.slice(1, 4);
+// var arr2 = arr.slice(1, 4);
 
 
-console.log(arr2.join(', '));
+// console.log(arr2.join(', '));
+
+// 6. forEach
+var angka = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// *function expression
+// e sebagai element pada array
+// var cetak = function(e) {
+//     console.log(e);
+// };
+
+// angka.forEach(cetak);
+
+// *function declaration
+// angka.forEach(function(e) {
+//     console.log(e);
+// });
+
+// Kita juga bisa pake index (i)
+// var nama = ['Galuh', 'Faiz', 'Mekel', 'Yuli', 'Ferry'];
+
+// var cetak = function(e, i) {
+//     console.log('Mahasiswa ke-' + (i+1) + ' adalah ' + e);
+// };
+
+// nama.forEach(cetak);
+
+// 7. map (Hampir sama dengan forEach tapi dia mengembalikan nilainya array juga yg mana ga bisa kalau pake forEach)
+// var angka = [1, 3, 4, 5, 7, 8, 9];
+// var angka2 = angka.map(function(e) {
+//     return e * 2;
+// })
+
+// console.log(angka2.join(', '));
+
+// 8. sort
+// *mengurutkan dari karakter utamanya dulu
+// var angka = [1, 5, 4, 2, 7, 9, 23, 2];
+// angka.sort();
+
+// *untuk mengurutkan dengan benar gunakan function berikut
+// angka.sort(function(a,b) {
+//     return a-b;
+// })
+
+// console.log(angka.join(', '));
+
+// 9. filter (Mengembalikan banyak nilai) (Mengembalikan dalam bentuk array)
+var angka = [1, 5, 4, 2, 7, 9, 23, 2];
+// var angka2 = angka.filter(function(e) {
+//     // return e == 5;
+//     return e > 5;
+// });
+
+// console.log(angka2);
+
+
+// 10. find (Mengembalikan satu nilai)
+var angka2 = angka.find(function(e) {
+    return e > 5;
+})
+
+console.log(angka2); // hanya mengembalikan 1 nilai
